248153dd27ebd425bfddaf4afe8611aaaae7cb59
guidev1911 <guilhermereal1911@gmail.com>
2025-07-28 22:11:26 -0300

    exception handler

---
+++ src/main/java/org/example/EASY/ExceptionHandler1.java
@@ -0,0 +1,21 @@
+package org.example.EASY;
+
+import java.io.*;
+import java.util.*;
+
+public class ExceptionHandler1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x / y);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        sc.close();
+    }
+}
